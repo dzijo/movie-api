d21ef068eaa226542392d49cf534a9a13a74cf96
Matej Džijan <matej.dzijan@gmail.com>
2019-10-07 12:19:39 +0200

    Initiated contact with the api

--- movie-api.ts
+++ movie-api.ts
@@ -1,1 +1,16 @@
 console.log("F");
+
+var data = "{}";
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&api_key=2628d67d7605e8187e3b28f7a28b220b");
+
+xhr.send(data);
--- scripts/movie-api.js
+++ scripts/movie-api.js
@@ -1,3 +1,13 @@
 "use strict";
 console.log("F");
+var data = "{}";
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+xhr.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&api_key=2628d67d7605e8187e3b28f7a28b220b");
+xhr.send(data);
 //# sourceMappingURL=movie-api.js.map
