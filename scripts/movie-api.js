9c344bee7abc75a89fcb1decd3081dba2956f081
Matej Džijan <matej.dzijan@gmail.com>
2019-10-14 02:15:42 +0200

    Initial load and load button

--- scripts/movie-api.js
+++ scripts/movie-api.js
@@ -1,16 +1,50 @@
 "use strict";
 var api_key = "2628d67d7605e8187e3b28f7a28b220b";
 var data = "{}";
+var i;
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+        response = JSON.parse(this.responseText);
+    }
+});
 var response;
 var baseImageUrl = "https://image.tmdb.org/t/p/w780";
-window.onload = async function () {
+window.onload = function () {
     console.log("D!");
     var list_container = document.getElementById("list-container");
+    var load_button = document.getElementById("buttonLoad");
+    if (load_button != null) {
+        load_button.addEventListener("click", function () {
+            console.log("I'm in!");
+            let movies_to_load = 48;
+            for (let j = i; j < i + movies_to_load; j++) {
+                if (j % 20 == 0) {
+                    xhr.open('get', `https://api.themoviedb.org/3/movie/popular?page=${Math.floor(j / 20) + 1}&api_key=${api_key}`, false);
+                    xhr.send();
+                    console.log(response);
+                }
+                var row;
+                if (j % 3 == 0) {
+                    row = document.createElement('div');
+                    row.setAttribute("class", "row");
+                    list_container.appendChild(row);
+                }
+                if (row != undefined) {
+                    row.appendChild(makeMovie(response["results"][j % 20]));
+                }
+            }
+            i += movies_to_load;
+        });
+    }
     if (list_container != null) {
         console.log("F!");
-        for (let i = 0; i < 48; i++) {
+        for (i = 0; i < 48; i++) {
             if (i % 20 == 0) {
-                await request(`https://api.themoviedb.org/3/movie/popular?page=${Math.floor(i / 20) + 1}&api_key=${api_key}`);
+                xhr.open('get', `https://api.themoviedb.org/3/movie/popular?page=${Math.floor(i / 20) + 1}&api_key=${api_key}`, false);
+                xhr.send();
                 console.log(response);
             }
             var row;
@@ -25,25 +59,8 @@ window.onload = async function () {
         }
     }
 };
-function request(url) {
-    return new Promise(function (resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.withCredentials = false;
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === this.DONE) {
-                console.log(this.responseText);
-                response = JSON.parse(this.responseText);
-            }
-        });
-        xhr.ontimeout = function () {
-            reject('timeout');
-        };
-        xhr.open('get', url, true);
-        xhr.send();
-    });
-}
 function makeMovie(data) {
-    let imagePath = baseImageUrl + data["poster_path"], id = data["id"], language = data["original_language"], title = data["original_title"], rating = data["vote_average"];
+    let imagePath = baseImageUrl + data["poster_path"], id = data["id"], language = data["original_language"], title = data["title"], rating = data["vote_average"];
     //Language
     let languageH = document.createElement('h3');
     languageH.innerText = `Language: ${language}`;
